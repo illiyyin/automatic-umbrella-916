afa0aa286d4f3c7023e6be9048c31bcb8144c70c
illiyyin <iilliyyin@gmail.com>
2023-09-20 19:52:42 +0700

    drawer

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.marginBottom20}>Home Screen</Text>
+      <Button
+        style={styles.button}
+        title="Pergi ke Progate"
+        onPress={() => {
+          navigation.navigate('Progate')
+        }}
+      />
+    </View>
+  )
+}
+
+const ProgateScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Progate</Text>
+      <Text style={styles.marginBottom20}>Ayo belajar!</Text>
+      <Button title="Kembali" onPress={() => navigation.goBack()} />
+    </View>
+  )
+}
+
+const Drawer = createDrawerNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Progate" component={ProgateScreen} />
+        <Drawer.Screen name="Home" component={HomeScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  marginBottom20: {
+    marginBottom: 20,
   },
 });
